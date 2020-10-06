ce7959271a7f5ed8a6246eda1056d5dac9227ca3
Pedro Almeida <ip4dro@gmail.com>
2020-10-06 14:08:22 +0200

    Add SFSafariViewController conformance to inspectable protocol

---
+++ Sources/HierarchyInspector/Extensions/SFSafariViewController/SFSafariViewController+HierarchyInspectableProtocol.swift
@@ -0,0 +1,50 @@
+//
+//  SFSafariViewController+HierarchyInspectableProtocol.swift
+//  HierarchyInspector
+//
+//  Created by Pedro Almeida on 06.10.20.
+//
+
+import SafariServices
+
+// MARK: - HierarchyInspectorPresentable
+
+extension SFSafariViewController: HierarchyInspectorPresentable {
+    public var hierarchyInspectorManager: HierarchyInspector.Manager {
+        guard
+            let existingManager = Self.sharedHierarchyInspectorManager,
+            existingManager.hostViewController === self
+        else {
+            
+            let newManager = HierarchyInspector.Manager(host: self).then {
+                $0.shouldCacheViewHierarchySnapshot = false
+            }
+            
+            Self.sharedHierarchyInspectorManager = newManager
+            
+            return newManager
+        }
+        
+        return existingManager
+    }
+
+    public var hierarchyInspectorLayers: [HierarchyInspector.Layer] {
+        [
+            .staticTexts,
+            .stackViews,
+            .containerViews
+        ]
+    }
+}
+
+extension SFSafariViewController {
+    static var sharedHierarchyInspectorManager: HierarchyInspector.Manager? {
+        didSet {
+            guard let previousManager = oldValue else {
+                return
+            }
+
+            previousManager.invalidate()
+        }
+    }
+}
