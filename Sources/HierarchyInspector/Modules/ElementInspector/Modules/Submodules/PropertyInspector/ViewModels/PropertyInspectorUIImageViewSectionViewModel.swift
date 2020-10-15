86da4c59d5ad3a106b31e5bf898b7cdab13f7e1f
Pedro Almeida <ip4dro@gmail.com>
2020-10-15 22:42:22 +0200

    Add image view inspector panel

---
+++ Sources/HierarchyInspector/Modules/ElementInspector/Modules/Submodules/PropertyInspector/ViewModels/PropertyInspectorUIImageViewSectionViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  PropertyInspectorUIImageViewSectionViewModel.swift
+//  HierarchyInspector
+//
+//  Created by Pedro Almeida on 10.10.20.
+//
+
+import UIKit
+
+final class PropertyInspectorUIImageViewSectionViewModel: PropertyInspectorSectionViewModelProtocol {
+    
+    private enum Property: CaseIterable {
+        case image
+        case highlightedImage
+        case isHighlighted
+        case adjustsImageSizeForAccessibilityContentSizeCategory
+    }
+    
+    private(set) weak var imageView: UIImageView?
+    
+    init(imageView: UIImageView) {
+        self.imageView = imageView
+    }
+    
+    private(set) lazy var title: String? = "Image"
+    
+    private(set) lazy var propertyInpus: [PropertyInspectorInput] = Property.allCases.compactMap {
+        guard let imageView = imageView else {
+            return nil
+        }
+
+        switch $0 {
+            
+        case .image:
+            return .imagePicker(
+                title: "image",
+                image: imageView.image
+            ) { image in
+                imageView.image = image
+            }
+            
+        case .highlightedImage:
+            return .imagePicker(
+                title: "highlighted image",
+                image: imageView.highlightedImage
+            ) { highlightedImage in
+                imageView.highlightedImage = highlightedImage
+            }
+            
+        case .isHighlighted:
+            return .toggleButton(
+                title: "highlighted",
+                isOn: imageView.isHighlighted
+            ) { isHighlighted in
+                imageView.isHighlighted = isHighlighted
+            }
+            
+        case .adjustsImageSizeForAccessibilityContentSizeCategory:
+            return .toggleButton(
+                title: "adjusts image size",
+                isOn: imageView.adjustsImageSizeForAccessibilityContentSizeCategory
+            ) { adjustsImageSizeForAccessibilityContentSizeCategory in
+                imageView.adjustsImageSizeForAccessibilityContentSizeCategory = adjustsImageSizeForAccessibilityContentSizeCategory
+            }
+        }
+    }
+}
