46b52febba6a47a5532cda2ea2a5ca979401c57a
Pedro Almeida <ip4dro@gmail.com>
2021-04-15 08:56:08 +0200

    Make convenience public

--- Sources/HierarchyInspector/Extensions/Optional/Optional+NilOrEmpty.swift
+++ Sources/HierarchyInspector/Extensions/Optional/Optional+NilOrEmpty.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Optional where Wrapped: Collection {
+public extension Optional where Wrapped: Collection {
     
     var isNilOrEmpty: Bool {
         return self?.isEmpty ?? true
