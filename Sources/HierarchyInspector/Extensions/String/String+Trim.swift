574be27ac44b295ff26ae7f71e2edbfdce634161
Pedro Almeida <ip4dro@gmail.com>
2021-04-21 21:37:34 +0200

    Simplify String+Trim extension

--- Sources/HierarchyInspector/Extensions/String/String+Trim.swift
+++ Sources/HierarchyInspector/Extensions/String/String+Trim.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  String+Trim.swift
+//  HierarchyInspector
 //
 //  Created by Pedro on 15.04.21.
 //
@@ -11,10 +11,6 @@ extension String {
     var trimmed: String? {
         let trimmedString = trimmingCharacters(in: .whitespacesAndNewlines)
         
-        if trimmedString.isEmpty {
-            return nil
-        }
-        
-        return trimmedString
+        return trimmedString.isEmpty ? nil : trimmedString
     }
 }
