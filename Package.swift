2d5f6d8203bccb3a6b1d8938928d75eff94ec96c
Pedro Almeida <ip4dro@gmail.com>
2021-05-25 14:24:28 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ipedro/UIKeyCommandTableView.git", from: "0.1.2"),
-        .package(url: "https://github.com/ipedro/UIKeyboardAnimatable.git", from: "0.2.4")
+        .package(url: "https://github.com/ipedro/UIKeyboardAnimatable.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
