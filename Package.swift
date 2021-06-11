51fe80459f70b7fb2a359d11828f137ec39891db
Pedro Almeida <ip4dro@gmail.com>
2021-06-11 21:10:34 +0200

    Add UIKitOptions as an explicit dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ipedro/UIKeyCommandTableView.git", from: "0.1.2"),
-        .package(url: "https://github.com/ipedro/UIKeyboardAnimatable.git", from: "0.3.0")
+        .package(url: "https://github.com/ipedro/UIKeyboardAnimatable.git", from: "0.3.0"),
+        .package(url: "https://github.com/ipedro/UIKitOptions.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +27,8 @@ let package = Package(
             name: "Inspector",
             dependencies: [
                 ._byNameItem(name: "UIKeyCommandTableView", condition: nil),
-                ._byNameItem(name: "UIKeyboardAnimatable", condition: nil)
+                ._byNameItem(name: "UIKeyboardAnimatable", condition: nil),
+                ._byNameItem(name: "UIKitOptions", condition: nil)
             ],
             resources: [
                 .process("Resources")
