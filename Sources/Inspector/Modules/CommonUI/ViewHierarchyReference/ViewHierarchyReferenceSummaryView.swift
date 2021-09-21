b80bc526c4ce707575f0e1855df471dba75aff90
Pedro Almeida <ip4dro@gmail.com>
2021-09-21 10:07:42 +0200

    Fix breaking constraints

--- Sources/Inspector/Modules/CommonUI/ViewHierarchyReference/ViewHierarchyReferenceSummaryView.swift
+++ Sources/Inspector/Modules/CommonUI/ViewHierarchyReference/ViewHierarchyReferenceSummaryView.swift
@@ -158,7 +158,7 @@ final class ViewHierarchyReferenceSummaryView: BaseView {
         installView(
             contentView,
             .insets(ElementInspector.appearance.directionalInsets),
-            priority: .required
+            priority: .init(rawValue: 999)
         )
     }
 }
